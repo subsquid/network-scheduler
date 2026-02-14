376519b40ee214d1188fdcc6d922c208af00ec41
tmcgroul <safonov.a.r@mail.ru>
2026-02-14 07:28:05 +0530

    Extend scheduling status with datasets height

--- src/types/status.rs
+++ src/types/status.rs
@@ -1,11 +1,13 @@
 use serde::Serialize;
 
+use super::Dataset;
 use super::Worker;
 
 #[derive(Debug, Clone, Serialize)]
 pub struct SchedulingStatus {
     pub config: SchedulingStatusConfig,
     pub workers: Vec<Worker>,
+    pub datasets: Vec<Dataset>,
     pub assignment_timestamp_sec: u64,
 }
 
